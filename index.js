bb0d76e66defb93b29fe751eaa71564fbe3a0678
Abdulrahman Alamoudi <am.alamoudi>
2020-10-07 22:29:14 +0300

    added example

---
+++ index.js
@@ -0,0 +1,11 @@
+/* sources
+    https://opensource.adobe.com/spectrum-web-components/components/button
+    https://opensource.adobe.com/spectrum-web-components/components/theme
+    https://github.com/adobe/spectrum-web-components/tree/main/projects/example-project-webpack
+    https://github.com/Westbrook/swc-parcel
+*/
+
+import '@spectrum-web-components/theme/sp-theme.js';
+import '@spectrum-web-components/theme/src/themes.js';
+import '@spectrum-web-components/button/sp-button.js';
+import '@spectrum-web-components/button/sp-clear-button.js';
